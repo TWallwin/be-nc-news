34dfbc036fd44c7631c321a17050611053b3eb62
Tom wallwin <tomwallwin@toms-mbp.home>
2022-02-15 12:05:07 +0000

    started patch endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,10 +16,9 @@ describe("app", () => {
         expect(msg).toBe("invalid path");
       });
   });
-  //   test("status 500 - internal server error", () => {}); // TODO how to test??
 
   describe("/api/topics - GET", () => {
-    test("status 200 - ok", () => {
+    test("status 200 - responds with array representing topics table from database", () => {
       const expectedTopics = [
         //expected test data
         {
@@ -44,8 +43,8 @@ describe("app", () => {
         });
     });
   });
-  describe("/api/articles/article_id", () => {
-    test("status 200 - ok", () => {
+  describe("/api/articles/article_id - GET", () => {
+    test("status 200 - responds with article that matches article_id", () => {
       const article1 = {
         article_id: 1,
         title: "Living in the shadow of a great man",
@@ -79,5 +78,30 @@ describe("app", () => {
           expect(msg).toBe("bad request");
         });
     });
+  });
+  describe("/api/articles/article_id - PATCH", () => {
+    test("status 200 - updates article", () => {});
+    test("status 200 - responds with updated article", () => {
+      const expectedData = {
+        article_id: 1,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: "2020-07-09T20:11:00.000Z",
+        votes: 101
+      };
+      return request(app)
+        .patch("/api/articles/1")
+        .send({ inc_votes: "1" })
+        .expect(200)
+        .then(({ body: { article } }) => {
+          expect(article).toBe(expectedData);
+        });
+    });
+    test("status 400 - invalid article_id ", () => {});
+    test("status 404 - article not found", () => {});
+    test("status 400 - malformed body eg{}", () => {});
+    test("status 400 - body rejected by psql ie wrong type", () => {});
   });
 });
