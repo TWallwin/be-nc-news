6d2b48bbd90f900207c6da8657767bc73ec17d01
Tom wallwin <tomwallwin@toms-mbp.home>
2022-02-17 12:39:36 +0000

    finished commentcount5

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -44,7 +44,8 @@ describe("app", () => {
         author: "butter_bridge",
         body: "I find this existence challenging",
         created_at: "2020-07-09T20:11:00.000Z",
-        votes: 100
+        votes: 100,
+        comment_count: "11"
       };
 
       return request(app)
