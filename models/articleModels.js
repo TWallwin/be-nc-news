e2cf2957b6df3253a4de366eed55a6b4703b237a
Tom wallwin <tomwallwin@toms-mbp.home>
2022-02-17 13:06:25 +0000

    finished making changes

--- models/articleModels.js
+++ models/articleModels.js
@@ -6,8 +6,8 @@ exports.fetchArticleById = (id) => {
   }
   return db
     .query(
-      `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, COUNT(articles.article_id) AS comment_count FROM articles 
-    RIGHT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id=$1
+      `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, COUNT(comments.article_id) AS comment_count FROM articles 
+    LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id=$1
     GROUP BY articles.article_id;`,
       [id]
     )
